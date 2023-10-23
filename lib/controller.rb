7e8831b65d44e984406cd62406f7201110262d27
ho757uff <robindebessel.pro@gmail.com>
2023-10-23 18:51:38 +0200

    no onto 2.6 - merci Louis et Lalaina <3

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,25 @@
+require_relative "gossip"
+require "pry"
+
 class ApplicationController < Sinatra::Base
   get "/" do
-    erb :index
+    erb :index, locals: {gossips: Gossip.all}
   end
 
   get "/gossips/new/" do
     erb :new_gossip
   end
+
+  post "/gossips/new/" do
+    gossip_author = params["gossip_author"]
+    gossip_content = params["gossip_content"]
+
+    new_gossip = Gossip.new(gossip_author, gossip_content)
+    new_gossip.save
+
+    puts "Merci pour ton potin copain !" # Ne sert à rien ! Ça va trop vite !
+    redirect "/gossips/new/" # ou redirect "/"
+  end
 end
+
+# binding.pry
---
+++ lib/gossip.rb
@@ -0,0 +1,25 @@
+require "csv"
+
+class Gossip
+  attr_accessor :author, :content
+
+  def initialize(author, content)
+    @content = content
+    @author = author
+  end
+
+  def save
+    CSV.open("./db/gossip.csv", "ab") do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    all_gossips
+  end
+end # End of class - "Gossip"
+# End of file - "gossip.rb"
